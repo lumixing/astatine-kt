ce667a0e51fcb1feab392a279a26970b4024103b
lumix <over100stars@gmail.com>
2023-02-09 22:56:13 +0200

    added splash texts!

--- lwjgl3/src/main/kotlin/dev/lumix/astatine/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/dev/lumix/astatine/lwjgl3/Lwjgl3Launcher.kt
@@ -10,7 +10,29 @@ import dev.lumix.astatine.engine.Static
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
     Lwjgl3Application(Astatine(), Lwjgl3ApplicationConfiguration().apply {
-        setTitle("astatine - rewritten! (pre-pre-pre-alpha)")
+        val splashTexts = arrayOf(
+            "now in kotlin!",
+            "i lost count..",
+            "by lumix!",
+            "currently radioactive",
+            "VERY in development",
+            "open source",
+            "mfbot would be proud",
+            "has *some* bugs",
+            "without log4j, i think",
+            "with chunk culling optimizations",
+            "with block entities overlap optimization",
+            "null",
+            "undefined",
+            "ArrayIndexOutOfBoundsException",
+            "*insert splash text here*",
+            "made with some love",
+            "now with splash texts!",
+            "im in a window title!",
+            "view is cool from up here"
+        )
+
+        setTitle("astatine v0.00∞ - ${splashTexts.random()}")
         setWindowedMode(Static.WIDTH, Static.HEIGHT)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
